da06c3e1d568d11ae8a6628be9872efb895baa8f
AyushUnleashed <myzealouslifeofficial@gmail.com>
2023-09-25 15:43:43 +0530

    Added draft email/ created main.py

---
+++ google_auth.py
@@ -0,0 +1,22 @@
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+SCOPES = ['https://mail.google.com/']
+
+def get_credentials(token_file='token.json', client_secrets_file='client_secrets_scribe_sphere.json'):
+    creds = None
+    if os.path.exists(token_file):
+        creds = Credentials.from_authorized_user_file(token_file, SCOPES)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(client_secrets_file, SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open(token_file, 'w') as token:
+            token.write(creds.to_json())
+
+    return creds
